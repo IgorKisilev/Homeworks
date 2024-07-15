21e70ee6f6dc98a188422ee70718ad1323bef444
IgorKisilev <igor_kisilev@mail.ru>
2024-07-15 22:56:58 +0300

    Add files via upload

---
+++ Homework 3.py
@@ -0,0 +1,12 @@
+name = 'Igor'
+print('Name: ' + name)
+age = 57
+age = str(age)
+print('Age: ' + age)
+age = int(age)
+age = age + 1
+age = str(age)
+print('New Age: ' + age)
+is_student = True
+is_student = str(is_student)
+print('Is Student:',is_student)
