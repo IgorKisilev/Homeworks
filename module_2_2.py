cfe430452755f6499275db1339a4e2719460ca4d
IgorKisilev <igor_kisilev@mail.ru>
2024-07-22 13:54:57 +0300

    Delete module_2_2.py

--- module_2_2.py
+++
@@ -1,9 +0,0 @@
-first = int(input())
-second = int(input())
-third = int(input())
-if first == second == third:
-    print(3)
-elif first ==second != third or first != second == third or first == third != second:
-    print(2)
-else:
-    print(0)
